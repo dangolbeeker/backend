16dc3eb98a9dac9c01d887604e996f1143c9ba70
Dave Irwin <david@redirwin.com>
2019-08-25 18:38:14 -0700

    Prepping for initial deploy.

--- api/server.js
+++ api/server.js
@@ -11,6 +11,10 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.status(200).json({ testObject: process.env.TEST });
+});
+
 server.use("/api/auth", authRouter);
 // server.use("/api/user/:id", authenticate);
 
