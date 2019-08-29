9992dc1d66c2aad5b0928493050236226b14f817
dangolbeeker <scottb305@outlook.com>
2019-08-29 15:58:37 -0400

    added questions to backend for game logic

---
+++ questions.js
@@ -0,0 +1,51 @@
+
+const questions = [
+    
+
+    {
+        
+        id: 1,
+        verb: "hablar",
+        tense: "Conditional",
+        parts: ["Nuestro Presidente dijo anteriormente que hablar de la decoración."],
+        response: "Our President said earlier that talk about decoration."
+    },
+
+    {
+        
+        id: 2,
+        verb: "hablar",
+        tense: "Future",
+        parts: ["Todos sabemos que se hablar de ello en la reunión del Consejo Europeo."],
+        response: "We all know that we will talk about it at the European Council meeting."
+    },
+
+    {
+        
+        id: 3,
+        verb: "hablar",
+        tense: "Preterite",
+        parts: ["Personalmente hablar al menos con tres Comisarios, los responsables de comercio, agricultura y seguridad alimentaria."],
+        response: "I personally speak with at least three Commissioners, those responsible for trade, agriculture and food security."
+    },
+
+    {
+       
+        id: 4,
+        verb: "hablar",
+        tense: "Imperfect",
+        parts: ["Me gustaría viajar al pasado para ver cómo hablar la gente."],
+        response: "I would like to travel to the past to see how people speak."
+    },
+
+    {
+        
+        id: 5,
+        verb: "hablar",
+        tense: "Present",
+        parts: ["Pensado que yo no afirmar nada, señor comisario, y que yo sólo sospecho."],
+        response: "Thought that I do not affirm anything, Commissioner, and that I only suspect."
+    }
+];
+
+module.exports = questions;
