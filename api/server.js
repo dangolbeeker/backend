2853a9bf311c410a674d10ff1b259640ef878a48
dangolbeeker <scottb305@outlook.com>
2019-08-29 23:51:53 -0400

    added questions seed working on route cant pass object to routing directly? push for codesandbox

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const authenticate = require("../auth/auth-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
 const verbsRouter = require("../verbs/verbs-router.js");
-const questions = require('./questions');
+const questions = require('../questions.js');
 
 const server = express();
 
@@ -21,6 +21,6 @@ server.get("/", (req, res) => {
 server.use("/api/auth", authRouter);
 server.use("/api/users", authenticate, usersRouter);
 server.use("/api/verbs", verbsRouter);
-server.use("/api/questions", questions);
+server.use("/api/questions", questions, questionsRouter);
 
 module.exports = server;
--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
+
+
+module.exports = router;
